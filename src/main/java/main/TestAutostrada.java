47dd5132e8c3de1ae876622b03bf1154c94b7aff
Samuel Finocchio <samuelfinocchioit@gmail.com>
2019-07-19 22:39:28 +0200

    Added new version of the project including maven and jdbc with old models included

--- src/main/java/main/TestAutostrada.java
+++ src/main/java/main/TestAutostrada.java
@@ -8,18 +8,26 @@ import java.util.*;
 import autostrada.*;
 import veicolo.*;
 import utility.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /**
  * @author Luca
  *
  */
-public class TestAutostrada {
+public class TestAutostrada extends Application {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
 		Map<Integer,Float> tariffaA25 = new HashMap<Integer,Float>();
 		List<Casello> listCasello = new ArrayList<Casello>();
 		Autostrada autostrada = new Autostrada("A25", tariffaA25, listCasello, Constants.PEDAGGIO_KM, 22);
@@ -43,5 +51,24 @@ public class TestAutostrada {
 		System.out.println("Il pedaggio per il veicolo e': "+ autostrada.stampaPedaggio(veicolo, casello1, casello2)+"0€");
 	
 	}
+	
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 
 }
