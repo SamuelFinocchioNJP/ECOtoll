754e835e812d6c5ecdb8e11dd852ba5be1d4bf97
Samuel Finocchio <samuelfinocchioit@gmail.com>
2019-07-20 00:16:38 +0200

    Added database utility class to return singleton and settings package with config class for db and other configurations

---
+++ src/main/java/Settings/Config.java
@@ -0,0 +1,8 @@
+package Settings;
+
+public class Config {
+	public static final String DB_CONNECTION_SERVER = "localhost";
+	public static final String DB_CONNECTION_DBNAME = "goontheroad";
+	public static final String DB_CONNECTION_DBUSERNAME = "root";
+	public static final String DB_CONNECTION_DBPASSWORD = "";
+}
