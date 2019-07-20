f4c3fe31c0c5581e19a301274acf50f7cc8c8242
Nazgot <p.ciammaricone@gmail.com>
2019-07-20 12:03:09 +0200

    Updated project settings to JRE 1.8.201, fixed warings and deprecated functions are now replaced, removed uselesse imports

--- src/main/java/home/Controller.java
+++ src/main/java/home/Controller.java
@@ -1,16 +1,12 @@
 package home;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
