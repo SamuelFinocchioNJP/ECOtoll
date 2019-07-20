971501d9ac9befae2803ebd9d4de453e152f6557
Luca <lucalanci98@gmail.com>
2019-07-20 14:40:38 +0200

    commented exceptions

--- src/main/java/veicolo/Classe4.java
+++ src/main/java/veicolo/Classe4.java
@@ -15,12 +15,12 @@ public class Classe4 extends Veicolo {
 	//Costruttore fino al 2021
 	public Classe4(String targa, String marca, String modello, int annoImmatricolazione, int assi) {
 		super(targa,marca,modello,annoImmatricolazione,assi);
-		if(annoImmatricolazione > 2021) throw new IllegalArgumentException("Costruttore errato per questo anno di immatricolazione");
+		//if(annoImmatricolazione > 2021) throw new IllegalArgumentException("Costruttore errato per questo anno di immatricolazione");
 	}
 	
 	//Costruttore dal 2021
 	public Classe4(String targa, String marca, String modello, int annoImmatricolazione, int assi, String classeAmbientale, int inquinamentoAcustico) {
 		super(targa, marca, modello, annoImmatricolazione, assi, classeAmbientale, inquinamentoAcustico);
-		if(annoImmatricolazione <= 2021) throw new IllegalArgumentException("Costruttore errato per questo anno di immatricolazione");
+		//if(annoImmatricolazione <= 2021) throw new IllegalArgumentException("Costruttore errato per questo anno di immatricolazione");
 	}
 }
--- src/main/java/veicolo/ClasseB.java
+++ src/main/java/veicolo/ClasseB.java
@@ -16,13 +16,13 @@ public class ClasseB extends Veicolo {
 	//Costruttore fino al 2021
 	public ClasseB(String targa, String marca, String modello, int annoImmatricolazione, int assi) {
 		super(targa,marca,modello,annoImmatricolazione,assi);
-		if(annoImmatricolazione > 2021) throw new IllegalArgumentException("Costruttore errato per questo anno di immatricolazione");
+		//if(annoImmatricolazione > 2021) throw new IllegalArgumentException("Costruttore errato per questo anno di immatricolazione");
 	}
 	
 	//Costruttore dal 2021
 	public ClasseB(String targa, String marca, String modello, int annoImmatricolazione, int assi, String classeAmbientale, int inquinamentoAcustico) {
 		super(targa, marca, modello, annoImmatricolazione, assi, classeAmbientale, inquinamentoAcustico);
-		if(annoImmatricolazione <= 2021) throw new IllegalArgumentException("Costruttore errato per questo anno di immatricolazione");
+		//if(annoImmatricolazione <= 2021) throw new IllegalArgumentException("Costruttore errato per questo anno di immatricolazione");
 	}
 
 }
