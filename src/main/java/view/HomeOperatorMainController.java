d85fda93523ca1a6c969505a9d00d7209c9fc97d
Luca <lucalanci98@gmail.com>
2019-07-21 17:55:02 +0200

    HomeOperatorMainController

--- src/main/java/view/HomeOperatorMainController.java
+++ src/main/java/view/HomeOperatorMainController.java
@@ -4,20 +4,32 @@ package view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import pedaggio.IPedaggio;
+import pedaggio.PedaggioKm;
+import utility.Constants;
 
 public class HomeOperatorMainController implements Initializable{
 
 	private String code;
+	private int toll_code;
+	private IPedaggio toll;
 	
 	@FXML
 	private Button btn_calculate;
 	
 	@FXML
 	private Label lbl_tollbooth;
+	
+	@FXML
+	private Button btn_km_toll;
+	
+	@FXML
+	private Button btn_eco_toll;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -32,11 +44,40 @@ public class HomeOperatorMainController implements Initializable{
 		return;
 	}
 	
-
-	public void onClick()
-	{
-		//TODO: Codice per prendere da file i dati, calcolare il prezzo e stamparlo
+	public void handleTollClick(ActionEvent actionEvent) {
 		
+		if(actionEvent.getSource() == btn_km_toll) {
+			toll_code = Constants.KM_TOLL;
+		}
+		
+		if(actionEvent.getSource() == btn_eco_toll) {
+			toll_code = Constants.ECO_TOLL;
+		}
+		System.out.println("Toll Selected: "+toll_code);
 		
 	}
+
+	public void onClick() {
+		
+		switch(toll_code) {
+		
+		case Constants.KM_TOLL:
+			//pedaggio per km
+			toll = new PedaggioKm();
+			toll.calcoloPedaggio(veicolo, puntoPagamentoIn, puntoPagamentoOut, listCasello, tariffaUnitaria, iva)
+			System.out.println(toll_code);
+			break;
+		
+		case Constants.ECO_TOLL:
+			// pedaggio eco
+			System.out.println(toll_code);
+			
+			break;
+		
+		default:
+			
+		}
+	}
+	
+	
 }
