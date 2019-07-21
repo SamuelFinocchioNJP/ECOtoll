bd36b7ca6e8bef3104e0bf925e8d48545780bc57
nazgot <p.ciammaricone@gmail.com>
2019-07-21 19:44:15 +0200

    Implemented delete method for tollbooths

--- src/main/java/view/TollboothInsertController.java
+++ src/main/java/view/TollboothInsertController.java
@@ -7,7 +7,6 @@ import Casello.Casello;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
