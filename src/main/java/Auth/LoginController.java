6905f2131911f2c876c4ee0dd287f9d93a562dce
Samuel Finocchio <samuelfinocchioit@gmail.com>
2019-07-21 20:13:43 +0200

    Created Auth package with Login Controller

---
+++ src/main/java/Auth/LoginController.java
@@ -0,0 +1,27 @@
+package Auth;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import utility.Database;
+
+public class LoginController {
+	
+	public boolean login ( String username, String password ) {
+		
+		ResultSet rs = null;
+		try {
+			rs = Database.getConnectionStatement()
+						.executeQuery( "SELECT * FROM administrator WHERE username='" + username + "', AND password = '" + password + "'" );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			return rs.next();
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+}
